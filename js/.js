3bc4067ab78fac198b6522c70ddf1be9556266bc
Anastasiia Zhylina <anastasiiazhylina@gmail.com>
2021-05-01 20:54:37 +0200

    modul 3

--- js/.js
+++ js/.js
@@ -293,4 +293,25 @@ function getProductPrice(productName) {
 }
 
 
-19. 
+19. const products = [
+  { name: 'Радар', price: 1300, quantity: 4 },
+  { name: 'Сканер', price: 2700, quantity: 3 },
+  { name: 'Дроид', price: 400, quantity: 7 },
+  { name: 'Захват', price: 1200, quantity: 9 },
+];
+
+function getAllPropValues(propName) {
+  // Пиши код ниже этой строки
+  
+  const propValues = [];
+  for (const product of products) {
+    if (product[propName]) {
+      propValues.push(product[propName])
+    }
+  }
+return propValues
+  // Пиши код выше этой строки
+}
+ console.log (products)
+
+20. 
